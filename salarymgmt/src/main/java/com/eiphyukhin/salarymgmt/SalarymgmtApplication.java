33f261eae888737c86445a13fac364e02ebe8581
yvonnekhin <yvonnekhin@hotmail.com>
2023-01-16 12:27:49 +0800

    initial spring boot app

---
+++ salarymgmt/src/main/java/com/eiphyukhin/salarymgmt/SalarymgmtApplication.java
@@ -0,0 +1,13 @@
+package com.eiphyukhin.salarymgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalarymgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalarymgmtApplication.class, args);
+	}
+
+}
---
+++ salarymgmt/src/test/java/com/eiphyukhin/salarymgmt/SalarymgmtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eiphyukhin.salarymgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalarymgmtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
